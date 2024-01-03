f25cc58eaa700f0f984bd14dfbd727517814ff69
jballinger34 <104314894+jballinger34@users.noreply.github.com>
2024-01-03 17:05:16 +0000

    Jamie Branch

--- PumpkinMMO/src/main/java/me/fakepumpkin7/pumpkinmmo/PumpkinMMO.java
+++ PumpkinMMO/src/main/java/me/fakepumpkin7/pumpkinmmo/PumpkinMMO.java
@@ -4,6 +4,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PumpkinMMO extends JavaPlugin {
 
+    //TODO
+    // PLAYER HAS DIFFERENT SKILLS
+    // COMBAT
+    // FARMING
+    // MINING
+    // FISHING
+    //
+    // WOODCUTTING/FORAGING
+    // ALCHEMY
+    //
+
+    // SKILLS STORED ON DISK/DB,
+    // WHEN PLAYER LOADS INTO SERVER
+    // PLAYERS SKILLS LOADED INTO RAM
+    // WHERE THEY CAN BE LEVELLED UP,
+    // AND THEY GAIN THE EFFECTS OF THE SKILLS
+
+
     @Override
     public void onEnable() {
         // Plugin startup logic
---
+++ PumpkinMMO/src/main/java/me/fakepumpkin7/pumpkinmmo/skills/Skill.java
@@ -0,0 +1,27 @@
+package me.fakepumpkin7.pumpkinmmo.skills;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public abstract class Skill {
+
+    @Getter @Setter
+    String name;
+
+
+
+
+    public Skill(){
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
