acc7d3ab9b7cb73b458462b3e63da64dd3c9ace2
jballinger34 <104314894+jballinger34@users.noreply.github.com>
2024-06-08 21:21:40 +0100

    Added core module with some ideas to implement

---
+++ PumpkinCore/src/main/java/me/fakepumpkin7/pumpkincore/PumpkinCore.java
@@ -0,0 +1,28 @@
+package me.fakepumpkin7.pumpkincore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PumpkinCore extends JavaPlugin {
+
+    //clear lag
+    //handle reboot
+    //scoreboard
+    //tablist
+    //format chat
+    //defence/health over hotbar
+    //combat tag
+    //permissions?
+
+
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
