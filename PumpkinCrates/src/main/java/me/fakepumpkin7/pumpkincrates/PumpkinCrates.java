1966d02a2cc6c2478f58c7c178c76eac9822c95f
jballinger34 <104314894+jballinger34@users.noreply.github.com>
2024-07-22 21:34:03 +0100

    Added PumpkinCrates module

---
+++ PumpkinCrates/src/main/java/me/fakepumpkin7/pumpkincrates/PumpkinCrates.java
@@ -0,0 +1,17 @@
+package me.fakepumpkin7.pumpkincrates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PumpkinCrates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
