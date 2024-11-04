b05537c33eeb25b5a8b6894f0d913f3c445b7b7a
jballinger34 <104314894+jballinger34@users.noreply.github.com>
2024-11-04 00:40:58 +0000

    Removed GUI menu package, i will rewrite this.

--- PumpkinFramework/src/main/java/me/fakepumpkin7/pumpkinframework/gui/actions/CloseAction.java
+++
@@ -1,8 +0,0 @@
-package me.fakepumpkin7.pumpkinframework.gui.actions;
-
-import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryCloseEvent;
-
-public interface CloseAction {
-    void close(Player player, InventoryCloseEvent event);
-}
